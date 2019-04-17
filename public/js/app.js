ff4f22f25d5059a891e950d4f401fbcae5e0cd78
Frank <fr.heggeness@gmail.com>
2019-04-16 15:01:38 -1000

    doneeeeeeeeee

--- public/js/app.js
+++ public/js/app.js
@@ -33,19 +33,33 @@ function changeCheckedStatus(idx, checkbox) {
 
 function removeItemButtonClicked(idx) {
   cart.removeItem(cart.items[idx]);
-  // let output = cart.render();
-  // content.innerHTML = output;
-  let list = document.getElementById('list');
-  list.removeChild(list.childNodes[idx]);
 
-  // let findXButton = document.getElementsByClassName('xButton');
-  // for (let i = 0; i < findXButton.length; i++) {
-  //   findXButton[i].addEventListener('click', function() {
-  //     removeItemButtonClicked(i);
-  //   });
-  // }
+  let list = document.getElementById('list');
 
   let findCheckboxes = document.getElementsByClassName('checkbox');
+
+  if (idx === 0 && list.length === 1) {
+    content.innerHTML = '';
+  } else {
+    list.removeChild(list.childNodes[idx]);
+    let newList = list.innerHTML;
+    list.innerHTML = newList;
+    for (let i = 0; i < cart.items.length; i++) {
+      if (cart.items[i].isDone === true) {
+        findCheckboxes[i].checked = true;
+      }
+    }
+  }
+
+  let findXButton = document.getElementsByClassName('xButton');
+  for (let i = 0; i < findXButton.length; i++) {
+    findXButton[i].removeEventListener('click', function() {
+      removeItemButtonClicked(i + 1);
+    });
+    findXButton[i].addEventListener('click', function() {
+      removeItemButtonClicked(i);
+    });
+  }
 
   for (let i = 0; i < findCheckboxes.length; i++) {
     findCheckboxes[i].addEventListener('change', function() {
